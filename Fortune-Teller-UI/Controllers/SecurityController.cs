63712d3a3ce73fb0870534a5608edb6d3cf1c97f
Corn Pivotal <cmendoza@pivotal.io>
2018-05-14 00:12:35 -0500

    Updates

---
+++ Fortune-Teller-UI/Controllers/SecurityController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FortuneTeller.Views
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            var results = new List<string>();
+            for (double s = 0; s < 1; s += .02d)
+            {
+                //results.Add(_utils.HexColorFromDouble(s));
+            }
+            return View(results);
+        }
+    }
+}
