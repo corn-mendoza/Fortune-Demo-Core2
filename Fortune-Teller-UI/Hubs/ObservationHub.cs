77b1c5d0afd548c939a073c0d16fe2fccd102993
Corn Pivotal <cmendoza@pivotal.io>
2018-05-14 23:02:45 -0500

    Updates for Security section

---
+++ Fortune-Teller-UI/Hubs/ObservationHub.cs
@@ -0,0 +1,25 @@
+﻿//using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace FortuneTeller.Hubs
+{
+    //public class ObservationHub : Hub
+    //{
+    //    public static int ConnectedClients = 0;
+
+    //    public override Task OnConnectedAsync()
+    //    {
+    //        ConnectedClients++;
+    //        Console.WriteLine($"Client connected, total connected now: {ConnectedClients}");
+    //        return base.OnConnectedAsync();
+    //    }
+
+    //    public override Task OnDisconnectedAsync(Exception exception)
+    //    {
+    //        ConnectedClients--;
+    //        Console.WriteLine($"Client disconnected, total connected now: {ConnectedClients}");
+    //        return base.OnDisconnectedAsync(exception);
+    //    }
+    //}
+}
