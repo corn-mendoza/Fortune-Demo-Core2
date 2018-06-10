63fcb65b440ee720b926264e335a1e61289b1770
Corn <cmendoza@pivotal.io>
2018-06-10 07:22:14 -0700

    Update create services script

--- scripts/createCloudFoundryServices.cmd
+++ scripts/createCloudFoundryServices.cmd
@@ -7,4 +7,4 @@ cf create-service p-redis shared-vm myRedisService
 cf create-service p-circuit-breaker-dashboard standard myHystrixService
 cf create-service p-rabbitmq standard myRabbitMQService
 cf cups myOAuthService -p "{\"client_id\": \"myWorkshop\",\"client_secret\": \"mySecret\",\"uri\": \"uaa://login.system.testcloud.com\"}"
-cf cups AttendeeContext -p "{\"connectionstring": \"myconnectionstring\"}"
+cf cups AttendeeContext -p "{\"connectionstring\": \"myconnectionstring\"}"
